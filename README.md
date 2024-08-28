b8022af27af225be658090ecbb18a475abd23d40
Chris Wiggins <chris@chriswiggins.co.nz>
2024-08-28 12:10:06 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# PiKVM Tapo Plug
+
+This repo is a small showcase for how to use TP-Link Tapo P100 Devices (and others, send a PR!) to be used with PiKVM.
+
+Using the sample configuration (with 3 plugs) renders the following output:
+
+![Example](./images/example.jpg)
+
+## Prerequisites
+
+Your Tapo plugs must be registered and online. You'll need their IP addresses to then put into the PiKVM override file
+
+## Installation
+
+To install on the PiKVM, run the following commands:
+
+1. SSH in as root, or use the web terminal, then `su` and enter root password
+2. Go into rw mode: `rw`
+3. `mkdir -p /opt/tapo`
+4. `pacman -S python-pip`
+5. `pip3 install tapo --break-system-packages`
+6. Put main.py into `/opt/tapo/main.py`
+7. Update your `/etc/kvmd/override.yaml` to contain something similar to the example (`override.example.yaml`) in this repo.
+8. Put device back into ro mode: `ro`
+9. Restart kvmd: `systemctl restart kvmd`
+
+## Configuration
+
+The script also supports a "toggle" action, which turns the plug off, waits 2 seconds then turns it back on again. Update the `override.yaml` as necessary for this to function.
---
+++ main.py
@@ -0,0 +1,47 @@
+import asyncio
+import argparse
+
+from tapo import ApiClient
+
+parser = argparse.ArgumentParser(description='Tapo PiKVM Script')
+parser.add_argument('--username', dest='username', help='Tapi API Username', required=True)
+parser.add_argument('--password', dest='password', help='Tapi API Password', required=True)
+parser.add_argument('--ip', dest='ip', help='Tapo IP Address of P100', required=True)
+parser.add_argument('action', choices=['on', 'off', 'toggle', 'status'], help='Action to perform')
+
+async def main():
+    args = parser.parse_args()
+
+    def tapo_print(msg):
+        print(f"Tapo[{args.ip}]: " + msg)
+
+    client = ApiClient(args.username, args.password)
+    device = await client.p100(args.ip)
+
+    if args.action == 'off' or args.action == 'toggle':
+        tapo_print("Turning device off...")
+        await device.off()
+
+    if args.action == 'toggle':
+        tapo_print("Waiting 2 seconds...")
+        await asyncio.sleep(2)
+
+    if args.action == 'on' or args.action == 'toggle':
+        tapo_print("Turning device on...")
+        await device.on()
+
+
+    if args.action == 'status':
+        status = await device.get_device_info_json()
+        if status['device_on']:
+            tapo_print("Device is ON")
+            return 0
+        else:
+            tapo_print("Device is OFF")
+            return 1
+
+    return 0
+
+if __name__ == "__main__":
+    ret = asyncio.run(main())
+    exit(ret)
